77b21149a36f8673f9e661cae4bc3bc09c20a561
peachspeed@note <peachspeed04@gmail.com>
2024-09-24 10:05:45 +0900

    루프 반복 없앰

--- add_1__1.py
+++ add_1__1.py
@@ -5,12 +5,14 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
+#grass.draw_now(400, 30)
+
 
 def run_rectangle():
     print('RECTANGLE')
-    pass
+
+    clear_canvas_now()
+    character.draw_now(400, 300)
 
 def run_circle():
     print('CIRCLE')
@@ -20,6 +22,8 @@ def run_circle():
 while True:
     run_circle()
     run_rectangle()
+    break
+
 
 close_canvas()
 
